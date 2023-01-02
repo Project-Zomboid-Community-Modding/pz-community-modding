b31d8d4667ea840f3cb67993cdb232df31153149
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-01-02 01:05:35 -0500

    Update ContextMenu.lua

--- Contents/mods/!pz_commPatchDebugTools/media/lua/client/AUD/ContextMenu.lua
+++ Contents/mods/!pz_commPatchDebugTools/media/lua/client/AUD/ContextMenu.lua
@@ -1,14 +1,38 @@
 local function showModData(obj) ModDataDebugPanel.OnOpenPanel(obj) end
 
 
+local modDataContextClasses ={
+    ["IsoDeadBody"] = function(object) return object:getOutfitName() end,
+    ["IsoPlayer"] = function(object) return object:getUsername(true) end,
+    ["IsoZombie"] = function(object) return object:getOutfitName() end,
+    ["BaseVehicle"] = function(object) return object:getScript():getName() end,
+}
+
+
+local function fillModDataContextMenu(array,contextMenu)
+    for i=0,array:size()-1 do
+        local object = array:get(i)
+        local optionAdded = false
+        for type,func in pairs(modDataContextClasses) do
+            if instanceof(object, type) then
+                local optionName = func(object)
+                if optionName then optionName = ": "..optionName end
+                contextMenu:addOption(type..(optionName or ""), object, showModData)
+                optionAdded = true
+            end
+        end
+
+        if not optionAdded then contextMenu:addOption(tostring(object:getClass()), object, showModData) end
+    end
+end
+
+
 local function AUDContextMenu(player, context, worldobjects, test)
     local sq = nil
-
 
     local mainMenu = context:addOptionOnTop("Inspect ModData", worldobjects, nil)
     local subMenu = ISContextMenu:getNew(context)
     context:addSubMenu(mainMenu, subMenu)
-
 
     for i,object in ipairs(worldobjects) do
         local square = object:getSquare()
@@ -27,46 +51,43 @@ local function AUDContextMenu(player, context, worldobjects, test)
                 customName = ": "..spriteName or tostring(object:getClass())
             end
 
-            subMenu:addOptionOnTop("IsoObject"..(customName or ""), object, showModData)
+            subMenu:addOption("IsoObject"..(customName or ""), object, showModData)
         end
     end
 
     if sq ~= nil then
         for x=sq:getX()-1, sq:getX()+1 do
             for y=sq:getY()-1, sq:getY()+1 do
-                local sq2 = getCell():getGridSquare(x, y, sq:getZ());
+                local sq2 = getCell():getGridSquare(x, y, sq:getZ())
                 if sq2 then
-                    for i=0,sq2:getMovingObjects():size()-1 do
-                        local object = sq2:getMovingObjects():get(i)
+                    local staticMovingObjects = sq2:getStaticMovingObjects()
+                    fillModDataContextMenu(staticMovingObjects,subMenu)
 
-                        if instanceof(object, "IsoPlayer") then
-                            local isoPlayerName = object:getUsername(true)
-                            if isoPlayerName then isoPlayerName = ": "..isoPlayerName end
-                            subMenu:addOption("IsoPlayer"..(isoPlayerName or ""), object, showModData)
-
-                        elseif instanceof(object, "IsoZombie") then
-                            local outfitName = object:getOutfitName()
-                            if outfitName then outfitName = ": "..outfitName end
-                            subMenu:addOption("IsoZombie"..(outfitName or ""), object, showModData)
-
-                        elseif instanceof(object, "BaseVehicle") then
-                            subMenu:addOption("Vehicle: "..object:getScript():getName(), object, showModData)
-                        end
-                    end
+                    local movingObjects = sq2:getMovingObjects()
+                    fillModDataContextMenu(movingObjects,subMenu)
                 end
             end
         end
     end
 
 end
-Events.OnFillWorldObjectContextMenu.Add(AUDContextMenu);
+Events.OnFillWorldObjectContextMenu.Add(AUDContextMenu)
 
 
 local function AUDInvContextMenu(player, context, items)
     for i,v in ipairs(items) do
-		local item = v;
-        if not instanceof(v, "InventoryItem") then item = v.items[1]; end
-        context:addOptionOnTop("Inspect Item ModData", item, showModData);
+		local item = v
+        if not instanceof(v, "InventoryItem") then item = v.items[1] end
+        context:addOptionOnTop("Inspect Item ModData", item, showModData)
 	end
 end
 Events.OnFillInventoryObjectContextMenu.Add(AUDInvContextMenu)
+
+
+
+local function bIs10YLActive()
+    local activeMods = getActivatedMods()
+    local modIDs = {"10YL_HIGH","10YL_MEDIUM","10YL_LOW"}
+    for _,mod in pairs(modIDs) do if activeMods:contains(mod) then return true end end
+    return false
+end
