d0f52f317aa0548b1630da93f40d27ac8fdf7238
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-01-19 00:43:04 -0500

    refactored button coloring for cheat buttons

--- Contents/mods/!pz_debugTools/media/lua/client/AUD/DebugPanel/MainTab.lua
+++ Contents/mods/!pz_debugTools/media/lua/client/AUD/DebugPanel/MainTab.lua
@@ -2,7 +2,7 @@ require("AUD/Init")
 require("")
 AUD.MainTabTable = {}
 
-AUDMainTab = ISPanelJoypad:derive("AUDMainTab")
+AUDMainTab = ISPanel:derive("AUDMainTab")
 
 
 local function decorateSetFunction(index, cheatPanel)
@@ -35,8 +35,14 @@ end
 
 local vehicleDebugFunctions = require "AUD/DebugPanel/VehicleTab/VehicleFunctions"
 
+local function cheatButtonSpecialFunc(button, cheatPanel, index)
+    if cheatPanel and index then
+        button.backgroundColor = cheatPanel.tickBox:isSelected(index) and AUD.Config.Buttons.GREEN or AUD.Config.Buttons.RED
+    end
+end
+
 function AUDMainTab:initialise()
-    ISPanelJoypad.initialise(self);
+    ISPanel.initialise(self);
 
     self:instantiate()
     self:setAnchorRight(true)
@@ -57,13 +63,13 @@ function AUDMainTab:initialise()
     for i=1,#cheatPanel.tickBox.options do
         local newFunc = decorateSetFunction(i, cheatPanel)
         local optionName = cheatPanel.tickBox.optionsIndex[i]
-        AUD.MainTabTable.addButton(self, newFunc, optionName, cheatPanel, i, x, y+(yStep*rows), AUD.Config.Buttons.Width, AUD.Config.Buttons.Height)
+        AUD.MainTabTable.addButton(self, newFunc, optionName, {cheatButtonSpecialFunc, cheatPanel, i}, x, y+(yStep*rows), AUD.Config.Buttons.Width, AUD.Config.Buttons.Height)
         rows = rows+1
     end
 
     for _,name_func in pairs(vehicleDebugFunctions) do
         local name, func = name_func()
-        AUD.MainTabTable.addButton(self, func, name, nil, nil, x, y+(yStep*rows), AUD.Config.Buttons.Width, AUD.Config.Buttons.Height)
+        AUD.MainTabTable.addButton(self, func, name, nil, x, y+(yStep*rows), AUD.Config.Buttons.Width, AUD.Config.Buttons.Height)
         rows = rows+1
     end
 
@@ -72,8 +78,15 @@ function AUDMainTab:initialise()
 end
 
 
-function AUD.MainTabTable.addButton(UIElement, setFunction, title, cheatPanel, index, x, y, width, height)
+function AUD.MainTabTable.addButton(UIElement, setFunction, title, specialFuncAndArgs, x, y, width, height)
     local btn = ISButton:new(x, y, width, height, title, nil, setFunction)
-    if cheatPanel and index then btn.backgroundColor = cheatPanel.tickBox:isSelected(index) and AUD.Config.Buttons.GREEN or AUD.Config.Buttons.RED end
+
+    local specialFunc = specialFuncAndArgs and specialFuncAndArgs[1]
+    if specialFunc and type(specialFunc) == "function" then
+        local args = {btn}
+        for i=2, #specialFuncAndArgs do table.insert(args,specialFuncAndArgs[i]) end
+        specialFunc(unpack(args))
+    end
+
     UIElement:addChild(btn)
 end
