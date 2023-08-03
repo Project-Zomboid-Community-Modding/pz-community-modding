f50e3821e1da3c242d60a0e68166bf02f2d2bc3a
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-08-03 08:23:56 -0400

    Create mutuallyExclusiveReaddingMPTraits_fix.lua
    Fixes #60

---
+++ Contents/mods/!pz_patch/media/lua/client/CommunityModdingPatch/mutuallyExclusiveReaddingMPTraits_fix.lua
@@ -0,0 +1,22 @@
+---https://github.com/Project-Zomboid-Community-Modding/pz-community-modding/issues/60
+
+require "OptionScreens/CharacterCreationProfession"
+--(trait:isRemoveInMP() and not isClient()
+local mutualyExclusive = CharacterCreationProfession.mutualyExclusive
+function CharacterCreationProfession:mutualyExclusive(trait, bAdd)
+    mutualyExclusive(self, trait, bAdd)
+
+    for i = 0, trait:getMutuallyExclusiveTraits():size() - 1 do
+        local exclusiveTrait = trait:getMutuallyExclusiveTraits():get(i)
+        exclusiveTrait = TraitFactory.getTrait(exclusiveTrait)
+
+        if exclusiveTrait:isRemoveInMP() and not isClient() then
+            if exclusiveTrait:getCost() > 0 then
+                self.listboxTrait:removeItem(exclusiveTrait:getLabel())
+            else
+                self.listboxBadTrait:removeItem(exclusiveTrait:getLabel())
+            end
+        end
+
+    end
+end
