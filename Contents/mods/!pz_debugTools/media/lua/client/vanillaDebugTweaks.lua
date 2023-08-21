3e6580edf58dbd49023e10682468014d73eed984
Poltergeistzx <igor.mov0@gmail.com>
2023-08-21 21:30:16 +0300

    patch getTexture

--- Contents/mods/!pz_debugTools/media/lua/client/vanillaDebugTweaks.lua
+++ Contents/mods/!pz_debugTools/media/lua/client/vanillaDebugTweaks.lua
@@ -131,3 +131,13 @@ do
         return close(self)
     end
 end
+
+do
+    ---ISItemsListViewer doesn't show icons for mod items because getTexture returns nil
+    ---trygetTexture needs nil check compared to original
+    local original = getTexture
+    getTexture  = function(fileName)
+        if fileName == nil then return nil end
+        return original(fileName) or Texture.trygetTexture(fileName)
+    end
+end
