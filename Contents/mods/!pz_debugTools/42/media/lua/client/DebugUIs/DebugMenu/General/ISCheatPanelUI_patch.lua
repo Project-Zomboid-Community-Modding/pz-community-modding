56dae4cf034fe3a8be73391dcdab6b42a53d6aae
SirDoggyJvla <simon.cadet79@hotmail.fr>
2025-10-05 19:09:37 +0200

    Cheat menu full rework

---
+++ Contents/mods/!pz_debugTools/42/media/lua/client/DebugUIs/DebugMenu/General/ISCheatPanelUI_patch.lua
@@ -0,0 +1,112 @@
+--[[
+
+Overrides parts of ISCheatPanelUI to use our own ISTickBoxCheatPanel, to customize the UI in a nicer way.
+This is basically a code rewrite of the previous method from the B41 version of the mod which broke in B42.12.0
+
+It goes in pair with ISTickBoxCheatPanel.lua
+
+]]--
+
+local ISTickBoxCheatPanel = require "ISUI/ISTickBoxCheatPanel"
+
+local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
+local FONT_HGT_MEDIUM = getTextManager():getFontHeight(UIFont.Medium)
+local UI_BORDER_SPACING = 10
+local BUTTON_HGT = FONT_HGT_SMALL + 6
+
+-- ISCheatPanelUI.instance = nil
+
+ISCheatPanelUI.persistentData = ISCheatPanelUI.persistentData or {}
+
+---Override to replace with our own ISTickBox (ISTickBoxCheatPanel) and change a bit the close button
+function ISCheatPanelUI:initialise()
+    self.backgroundColor = {r=0, g=0, b=0, a=0.5}
+    self.borderColor = {r=0.4, g=0.4, b=0.4, a=1}
+
+    ISPanel.initialise(self)
+    local btnWid = 100
+
+    self.ok = ISButton:new((self:getWidth()-btnWid)/2, self:getHeight() - UI_BORDER_SPACING - BUTTON_HGT - 1, btnWid, BUTTON_HGT, getText("IGUI_RadioClose"), self, ISCheatPanelUI.onClick)
+    self.ok.internal = "SAVE"
+    self.ok.anchorTop = false
+    self.ok.anchorBottom = true
+    self.ok:initialise()
+    self.ok:instantiate()
+    self.ok.borderColor = {r=1, g=1, b=1, a=0.5}
+    self.ok.backgroundColor = {r=0.1, g=0.1, b=0.1, a=0.5}
+    -- self.ok:enableAcceptColor()
+    self:addChild(self.ok)
+
+    self.tickBox = ISTickBoxCheatPanel:new(UI_BORDER_SPACING+1, UI_BORDER_SPACING+FONT_HGT_MEDIUM+1, self.width - (UI_BORDER_SPACING+1)*2, BUTTON_HGT, "Admin Powers", self, self.onTicked)
+    self.tickBox.choicesColor = {r=1, g=1, b=1, a=1}
+    self.tickBox:setFont(UIFont.Small)
+    self:addChild(self.tickBox)
+
+    self:addAdminPowerOptions()
+end
+
+---Override to save options when changed
+---@param index integer
+---@param selected boolean
+function ISCheatPanelUI:onTicked(index, selected)
+    if not self.player:isDead() then
+        for i,option in ipairs(ISCheatPanelUI.OptionList) do
+            option.player = self.player
+            option:setValue(self.tickBox:isSelected(i))
+        end
+        if isClient() then sendPlayerExtraInfo(self.player) end
+    end
+    self:saveOptions()
+end
+
+---Override to save x y position when closed
+function ISCheatPanelUI:setVisible(bVisible)
+    if not bVisible and ISCheatPanelUI.instance then
+        ISCheatPanelUI.persistentData = {
+            x = ISCheatPanelUI.instance.x,
+            y = ISCheatPanelUI.instance.y,
+        }
+    end
+    if self.javaObject == nil then
+		self:instantiate();
+	end
+	self.javaObject:setVisible(bVisible);
+    if self.visibleTarget and self.visibleFunction then
+        self.visibleFunction(self.visibleTarget, self);
+    end
+end
+
+---Override to close and open cheat menu from the ISDebugMenu Cheat button instead of the ok save button
+function ISCheatPanelUI.OnOpenPanel()
+    if ISCheatPanelUI.instance==nil then
+        local x,y = ISCheatPanelUI.persistentData.x, ISCheatPanelUI.persistentData.y
+        ISCheatPanelUI.instance = ISCheatPanelUI:new(x, y, 212+(getCore():getOptionFontSizeReal()*35), 350, getPlayer())
+        ISCheatPanelUI.instance:initialise()
+        ISCheatPanelUI.instance:addToUIManager()
+        ISCheatPanelUI.instance:setVisible(true)
+    else
+        ISCheatPanelUI.instance:setVisible(false)
+        ISCheatPanelUI.instance:removeFromUIManager()
+        ISCheatPanelUI.instance = nil
+    end
+
+    return ISCheatPanelUI.instance
+end
+
+---Override to set x y persistent data
+function ISCheatPanelUI:new(x, y, width, height, player)
+    local o = {}
+    x = x or getCore():getScreenWidth() - (width) - 50
+    y = y or getCore():getScreenHeight() / 4 - (height / 2)
+    o = ISPanel:new(x, y, width, height)
+    setmetatable(o, self)
+    self.__index = self
+    o.borderColor = {r=0.4, g=0.4, b=0.4, a=1}
+    o.backgroundColor = {r=0, g=0, b=0, a=0.8}
+    o.width = width
+    o.height = height
+    o.player = player
+    o.moveWithMouse = true
+    ISCheatPanelUI.instance = o
+    return o
+end
---
+++ Contents/mods/!pz_debugTools/42/media/lua/client/ISUI/ISTickBoxCheatPanel.lua
@@ -0,0 +1,99 @@
+---@class ISTickBoxCheatPanel : ISTickBox
+local ISTickBoxCheatPanel = ISTickBox:derive("ISTickBoxCheatPanel")
+
+---CACHE VALUES
+local UI_BORDER_SPACING = 10
+local goodHighlightedColor = getCore():getGoodHighlitedColor()
+local good_r, good_g, good_b = goodHighlightedColor:getR(), goodHighlightedColor:getG(), goodHighlightedColor:getB()
+local badHighlightedColor = getCore():getBadHighlitedColor()
+local bad_r, bad_g, bad_b = badHighlightedColor:getR(), badHighlightedColor:getG(), badHighlightedColor:getB()
+local colors = {
+    enabled = { r = good_r, g = good_g, b = good_b, a = 0.25 },
+    disabled = { r = bad_r, g = bad_g, b = bad_b, a = 0.25 },
+    hoverEnabled = { r = good_r, g = good_g, b = good_b, a = 0.5 },
+    hoverDisabled = { r = bad_r, g = bad_g, b = bad_b, a = 0.5 },
+}
+
+---Override to make width fit parent ISCheatPanelUI
+function ISTickBoxCheatPanel:setWidthToFit()
+    local iSCheatPanelUI = self.parent --[[@as ISCheatPanelUI]]
+	self.width = iSCheatPanelUI.width - UI_BORDER_SPACING*2
+end
+
+---Override to center options and change colors and placements
+function ISTickBoxCheatPanel:render()
+    -- get the option box sizes
+    local iSCheatPanelUI = self.parent --[[@as ISCheatPanelUI]]
+    local h = self.boxSize
+    local w = iSCheatPanelUI.width - (UI_BORDER_SPACING+1)*2
+
+    -- mostly kept from the original function
+	local y = 0
+	local c = 1
+	local totalHgt = #self.options * (self.itemHgt + UI_BORDER_SPACING) - UI_BORDER_SPACING
+	y = y + (self.height - totalHgt) / 2
+	local textDY = (self.itemHgt - self.fontHgt) / 2
+	local boxDY = 0
+	self._textColor = self._textColor or { r = 1, g = 1, b = 1, a = 1 }
+    for i,v in ipairs(self.options) do
+        -- get the option state
+        local disabled = not self:isSelected(i)
+        local over = self:isMouseOver() and (self.mouseOverOption == c)
+
+        -- select the box color based on the option state
+        local boxColor
+        if over then
+            boxColor = disabled and colors.hoverDisabled or colors.hoverEnabled
+        else
+            boxColor = disabled and colors.disabled or colors.enabled
+        end
+
+        -- draw the option box
+        self:drawRect(self.leftMargin, y+boxDY, w, h, boxColor.a, boxColor.r, boxColor.g, boxColor.b)
+        self:drawRectBorder(self.leftMargin, y+boxDY, w, h, boxColor.a, boxColor.r, boxColor.g, boxColor.b)
+
+        -- render the option text
+		local textColor = self._textColor
+		self:getTextColor(i, textColor)
+
+        -- used to calculate the centered position
+        local txtWidth = getTextManager():MeasureStringX(self.font, v)
+        local txtX = self.width/2 - txtWidth/2
+
+        -- draw text
+        self:drawText(v, txtX, y+textDY, textColor.r, textColor.g, textColor.b, textColor.a, self.font)
+
+        y = y + self.itemHgt + UI_BORDER_SPACING
+		c = c + 1
+    end
+
+    -- this handles the tooltip, kept from the original function
+    if self:isMouseOver() and self.mouseOverOption and self.mouseOverOption ~= 0 and self.tooltip then
+        local text = self.tooltip
+        if not self.tooltipUI then
+            self.tooltipUI = ISToolTip:new()
+            self.tooltipUI:setOwner(self)
+            self.tooltipUI:setVisible(false)
+            self.tooltipUI:setAlwaysOnTop(true)
+        end
+        if not self.tooltipUI:getIsVisible() then
+            if string.contains(self.tooltip, "\n") then
+                self.tooltipUI.maxLineWidth = 1000 -- don't wrap the lines
+            else
+                self.tooltipUI.maxLineWidth = 300
+            end
+            self.tooltipUI:addToUIManager()
+            self.tooltipUI:setVisible(true)
+        end
+        self.tooltipUI.description = text
+        self.tooltipUI:setX(self:getMouseX() + 23)
+        self.tooltipUI:setY(self:getMouseY() + 23)
+    else
+        if self.tooltipUI and self.tooltipUI:getIsVisible() then
+            self.tooltipUI:setVisible(false)
+            self.tooltipUI:removeFromUIManager()
+        end
+    end
+end
+
+return ISTickBoxCheatPanel
